0d90faf8a8d3ad6242b09d17b6644d7c514519be
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2019-07-16 10:25:47 +0300

    Add private_dns output variable
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

--- output.tf
+++ output.tf
@@ -10,3 +10,7 @@ output "private_ip" {
 output "public_ip" {
   value = "${aws_instance.ec2_instance.*.public_ip}"
 }
+
+output "private_dns" {
+  value = "${aws_instance.ec2_instance.*.private_dns}"
+}
