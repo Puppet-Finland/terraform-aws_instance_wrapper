1cd53647b802388c3d8017869f5e9a86475a16b7
Samuli Seppänen <samuli.seppanen@puppeteers.fi>
2020-10-23 15:32:14 +0300

    Set input variable datatypes more correctly
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

--- input.tf
+++ input.tf
@@ -3,12 +3,13 @@ variable "ami" {
 
 # Setting "amount" to 0 is equivalent to "ensure => absent" in Puppet
 variable "amount" {
+  type = number
   default = 1
 }
 
 variable "associate_public_ip_address" {
-  type    = string
-  default = "true"
+  type    = bool
+  default = true
 }
 
 variable "custom_provisioning_scripts" {
@@ -21,26 +22,30 @@ variable "default_root_block_device" {
 }
 
 variable "deployment" {
+  type = string
 }
 
 variable "ebs_optimized" {
-  type    = string
-  default = "true"
+  type    = bool
+  default = true
 }
 
 variable "disable_api_termination" {
-  type    = string
-  default = "true"
+  type    = bool
+  default = true
 }
 
 variable "hostname" {
+  type = string
 }
 
 variable "instance_type" {
+  type = string
   default = "t2.micro"
 }
 
 variable "key_name" {
+  type = string
   default = "terraform"
 }
 
@@ -50,30 +55,35 @@ variable "private_ip" {
 }
 
 variable "provisioning_ssh_key" {
+  type = string
 }
 
 variable "provisioning_user" {
+  type = string
   default = "ubuntu"
 }
 
 variable "provision_using_private_ip" {
-  type    = string
-  default = "false"
+  type    = bool
+  default = true
 }
 
 # By default use "deployment" as the Puppet environment name
 variable "puppet_environment" {
+  type = string
   default = "false"
 }
 
 variable "puppetmaster_ip" {
+  type = string
 }
 
 variable "region" {
-  #  type = string
+  type = string
 }
 
 variable "subnet_id" {
+  type = string
 }
 
 variable "tags" {
