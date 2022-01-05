58d0d3827921ecb33812ff2d1d373f29582245f7
Samuli Seppänen <samuli@openvpn.net>
2022-01-05 16:40:47 +0200

    Always set hostname, even if Puppet Agent is not installed
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
+  etc_hosts_command    = var.puppetmaster_ip == "" ? "echo Not modifying /etc/hosts" : "echo ${var.puppetmaster_ip} puppet|sudo tee -a /etc/hosts"
   deployment_tag       = { "deployment" = var.deployment }
   hostname_tag         = { "Name"       = var.hostname   }
 
@@ -8,9 +9,8 @@ locals {
                           "sudo chown -R root:root /etc/puppetlabs/facter",
                           "chmod +x /tmp/install-puppet.sh",
                           "sudo /tmp/install-puppet.sh -n ${var.hostname} -e ${local.puppet_env} -p ${var.puppet_version} -s"]
-
-  etc_hosts_command    = var.puppetmaster_ip == "" ? "echo Not modifying /etc/hosts" : "echo ${var.puppetmaster_ip} puppet|sudo tee -a /etc/hosts"
   puppet_env           = var.puppet_environment == "false" ? var.deployment : var.puppet_environment
+  set_hostname_command = "sudo hostnamectl set-hostname ${var.hostname}"
   sns_topic_arn        = var.sns_topic_arn == "none" ? "" : var.sns_topic_arn
   tags                 = merge(local.hostname_tag, local.deployment_tag, var.tags)
 }
