Puppet-Finland/terraform-aws_instance_wrapper

commit 1cd53647b802388c3d8017869f5e9a86475a16b7

Set input variable datatypes more correctly
Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.fi>

input.tf
@@ -3,12 +3,13 @@ variable "ami" {
 
 # Setting "amount" to 0 is equivalent to "ensure => absent" in Puppet
 variable "amount" {
+  type = number
   default = 1
 }
 
 variable "associate_public_ip_address" {
-  type    = string
-  default = "true"
+  type    = bool
+  default = true
 }
 
 variable "custom_provisioning_scripts" {
@@ -21,26 +22,30 @@ variable "default_root_block_device" {
 }
 
 variable "deployment" {
+  type = string
 }
 
 variable "ebs_optimized" {
-  type    = string
-  default = "true"
+  type    = bool
+  default = true
 }
 
 variable "disable_api_termination" {
-  type    = string
-  default = "true"
+  type    = bool
+  default = true
 }
 
 variable "hostname" {
+  type = string
 }
 
 variable "instance_type" {
+  type = string
   default = "t2.micro"
 }
 
 variable "key_name" {
+  type = string
   default = "terraform"
 }
 
@@ -50,30 +55,35 @@ variable "private_ip" {
 }
 
 variable "provisioning_ssh_key" {
+  type = string
 }
 
 variable "provisioning_user" {
+  type = string
   default = "ubuntu"
 }
 
 variable "provision_using_private_ip" {
-  type    = string
-  default = "false"
+  type    = bool
+  default = true
 }
 
 # By default use "deployment" as the Puppet environment name
 variable "puppet_environment" {
+  type = string
   default = "false"
 }
 
 variable "puppetmaster_ip" {
+  type = string
 }
 
 variable "region" {
-  #  type = string
+  type = string
 }
 
 variable "subnet_id" {
+  type = string
 }
 
 variable "tags" {

commit a058f2dbb5411fccf8e82d26f0d990dcb1d35796

Fix provision_using_private_ip
The check was broken probably since commit
1cd53647b802388c3d8017869f5e9a86475a16b7
("Set input variable datatypes more correctly")
changed the type from string to bool.

Also change default to false since that what it
effectively was all the time.

Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

input.tf
@@ -109,7 +109,7 @@ variable "provisioning_user" {
 
 variable "provision_using_private_ip" {
   type    = bool
-  default = true
+  default = false
 }
 
 # By default use "deployment" as the Puppet environment name

main.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "ec2_instance" {
     type        = "ssh"
     user        = var.provisioning_user
     private_key = file(var.provisioning_ssh_key)
-    host        = var.provision_using_private_ip == "true" ? aws_instance.ec2_instance[0].private_ip : aws_instance.ec2_instance[0].public_ip
+    host        = var.provision_using_private_ip == true ? self.private_ip : self.public_ip
   }
 
   provisioner "file" {