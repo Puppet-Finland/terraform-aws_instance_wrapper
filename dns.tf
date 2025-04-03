b5885e27e38d3bf141f74c5f60fabf4a5f9e6140
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2025-04-03 15:58:15 +0300

    Fix hosted zone support when var.amount == 0
    When this was the case, there was no aws_instance and hence the query
    for its private IP failed.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.net>

--- dns.tf
+++ dns.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "private_ipv4" {
-  count   = var.hosted_zone_id == "" ? 0 : 1
+  count   = var.hosted_zone_id != "" && var.amount == 1 ? 1 : 0
   zone_id = var.hosted_zone_id
   name    = split(var.hostname, ".")[0]
   type    = "A"
